bb3f537c2133ac7e54c9a19d812fe5ad161cada1
AhmadFel <i7mdia@hotmail.com>
2021-07-11 14:44:07 +0300

    vue project

--- resources/js/Components/Project.vue
+++ resources/js/Components/Project.vue
@@ -2,11 +2,26 @@
 <div>
     <h3>
         projects
-     </h3>   
-</div>    
+     </h3>     
+<ul>
+    <li v-for="project in projects" :key="project.id">
+        {{ project.title }}
+    </li>
+</ul>
+</div>
 </template>
 <script>
 export default{
+        data(){
+            return{
+                projects: []
+            }
 
+        },
+        beforCreate(){
+            axios.get('/api/project'),then((response)=>{
+                this.projects=response.data;
+            })
+        }
 }
 </script>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,4 @@
 @extends('layout')
 @section('content')
-    Welcome Page
+    <project />
 @endsection
